8b0503dc2431c918e8e9875c6fc62c27c5f3b46a
meilun <meicocacol@gmail.com>
2026-09-11 13:52:46 -0300

    feat:crear clase session y habit

---
+++ src/main/java/com/test/models/Habit.java
@@ -0,0 +1,41 @@
+package com.test.models;
+
+public class Habit {
+    private String name;
+    private int id;
+    private boolean done;
+    private int streak;
+    private static int count = 0;
+
+    Habit(String name, int streak){
+        this.name = name;
+        this.streak = streak;
+        done = false;
+        setTaskId(++count);
+    }
+
+    public int getId(){
+        return id;
+    }
+    public int getStreak(){
+        return streak;
+    }
+    public boolean getDone(){
+        return done;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setTaskId(int id){
+        this.id = id;
+    }
+
+    public void checkDonete(){
+        done = !done;
+    }
+}
---
+++ src/main/java/com/test/models/Session.java
@@ -0,0 +1,37 @@
+package com.test.models;
+
+public class Session {
+    private int duration;
+    private String topic;
+    private int count;
+
+    Session(int duration, String topic, int count){
+        this.duration = duration;
+        this.count = count;
+        this.topic = topic;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+    public int getCount() {
+        return count;
+    }
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public int calcTotal(){
+        return duration * count;
+    }
+}
